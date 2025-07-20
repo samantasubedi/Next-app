3641d7c11a5aff0393de0dd608fb8ceaeddc2bb4
samantasubedi <samantasubedi01@gmail.com>
2025-07-20 21:04:20 +0545

    post method

--- app/api/user/[id]/route.tsx
+++ app/api/user/[id]/route.tsx
@@ -21,8 +21,12 @@ export function GET(request: NextRequest, { params: { id } }: props) {
   else return NextResponse.json("the id doesnot exist");
 }
 export async function PUT(request:NextRequest, {params: { id } }: props) {
-  const body= await request.json()
-  if (!body.name){
+  const body= await request.json()    //data sent from frontend is received here
+ if(id>10){return(NextResponse.json({"error":"id too large"}))}
+  if (!body.name){                     //body is the data sent from fronted to be updated in the database
     return(NextResponse.json({error:"please provide a name field"},{status:400}))// checking if the request body is valid, request body is the data sent to be updated in the database we are checking if its format matches the format of the data stored as every data must me stored in same format
+  }
+  else {
+    return(NextResponse.json({"id":"5","name":"jonas kanawald"}))
   }
 }
