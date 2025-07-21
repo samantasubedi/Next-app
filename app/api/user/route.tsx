708a4e19a1c79863b5415779ccdd28eb9185514d
samantasubedi <samantasubedi01@gmail.com>
2025-07-21 22:58:21 +0545

    working with cloud

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -1,19 +1,11 @@
 //this is a route file , a folder can have either a page file which returns html elements or a route file which handles http requestsn
 import { NextRequest, NextResponse } from "next/server";
+ 
+import { prisma } from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    {
-      id: 1,
-      name: "samanta",
-      phone: 9700009945,
-      address: "kathmandu",
-    },
-    {
-      id: 2,
-      name: "harry",
-      phone: 9700009945,
-      address: "us",
-    },
-  ]);
+export async function GET(request: NextRequest) {
+
+const user = await prisma.user.findMany()
+  return NextResponse.json({user});
+
 }
